916a477a6b28fe829c8f8f605abebb6e3db87d98
prasanth <prasanth.chandrasekharapanicker@atkinsglobal.com>
2018-04-27 15:16:29 +0100

    Ejected create-react-app and webpack conf changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Viewer } from "cesium-react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Viewer  />       
+      </div>
+    );
+  }
+}
+
+export default App;
